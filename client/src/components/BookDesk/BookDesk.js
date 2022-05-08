ba59cb9118cf5ffa6e20b61ef5353d8fe3c59f84
Duane Imambaksh <dimambaksh@gmail.com>
2022-05-08 09:18:47 -0600

    Desk axios call

--- client/src/components/BookDesk/BookDesk.js
+++ client/src/components/BookDesk/BookDesk.js
@@ -4,11 +4,49 @@ import Button from "@mui/material/Button";
 import DateSelect from "../DateSelect/DateSelect";
 
 export default class BookDesk extends React.Component {
+  state = {
+    floorSelected: "",
+    dateSelected: "",
+  };
+
+  setFloorSelected = (stringIn) => {
+    this.setState({ floorSelected: stringIn });
+  };
+
+  setDateSelected = (stringIn) => {
+    this.setState({ dateSelected: stringIn });
+  };
+
+  
+  handleBookDeskDateFloor = (event) =>{
+    event.preventDefault();
+    event.stopPropagation();
+    this.setState({})
+}
+
+getDesksAvailable = () => {
+    axios({
+      method: "get",
+      url: `http://localhost:8080/reserve`,
+      data: {
+        date: this.state.dateSelected,
+        floor: this.state.floorSelected,
+      },
+    })
+      .then((response) => {
+          console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(`Server Response: ${error.response.data}`);
+      });
+  };
+
   render() {
     return (
       <div className="bookdeskcontainer">
         <h1>Book a Desk</h1>
-        <DateSelect dateFloorHandler={this.props.dateFloorHandler}></DateSelect>
+        <DateSelect dateFloorHandler={this.handleBookDeskDateFloor} setFloorSelected={setFloorSelected} setDateSelected={setDateSelected}></DateSelect>
         <Button
           variant="contained"
           onClick={(event) => {
