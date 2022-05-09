a7e0960a0248cc4446b8b546e3d4c117472a1b4c
Duane Imambaksh <dimambaksh@gmail.com>
2022-05-09 13:41:59 -0600

    Book confirmation with desk information

--- client/src/components/BookConfirmation/BookConfirmation.js
+++ client/src/components/BookConfirmation/BookConfirmation.js
@@ -1,7 +1,6 @@
 import "./BookConfirmation.scss";
 import React from "react";
 import Button from "@mui/material/Button";
-import { ReactComponent as ConfirmationOK } from "../../assets/icons/confirm-ok.svg";
 import { ReactComponent as ZoneIcon } from "../../assets/icons/zone.svg";
 import { ReactComponent as DeskIcon } from "../../assets/icons/desk.svg";
 import { ReactComponent as DateIcon } from "../../assets/icons/date.svg";
@@ -30,6 +29,20 @@ export default class BookConfirmation extends React.Component {
               fill="#0FA958"
             />
           </svg>
+        </div>
+        <div className="bookconfirmationcontainer__choice">
+          <div>
+            <ZoneIcon /> {this.props.deskSelected.zone}
+          </div>
+          <div>
+            <DeskIcon /> {this.props.deskSelected.desk}
+          </div>
+          <div>
+            <DateIcon /> {this.props.dateSelected}
+          </div>
+          <div>
+            <FloorIcon /> {this.props.deskSelected.floor}
+          </div>
         </div>
         <div className="buttonFlex">
         <Button
